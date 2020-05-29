a5e4d856f9a8eab615aedde0b00acf587bb83d02
Youngmin Park <codepark2019@gmail.com>
2020-05-29 06:17:23 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,10 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
-  while
+  counter = 0
+  while counter < array.length
+    yield array[counter]
+    counter += 1
+  end
+  array
+
 end
